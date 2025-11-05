cb1af7ccb487f3606defaaa20e07b330de29f397
chetanak10 <146982445+chetanak10@users.noreply.github.com>
2025-11-06 00:22:48 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Default to no Supabase; user will copy config.example.js -> config.js
-window.APP_CONFIG = window.APP_CONFIG || {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
-  DEFAULT_TEACHER_EMAIL: ""
-};
